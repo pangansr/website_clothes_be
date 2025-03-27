ba6d92f713ecf93f0da4085f0cf1703462b3c025
pangansr <phamthanhliem011203@gmail.com>
2025-03-28 01:52:14 +0700

    28/3 1:29 build

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["https://clothes-r3l3hz94x-liems-projects-1b3a9117.vercel.app"],
+    origin: ["https://clothes-eosin.vercel.app"],
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
